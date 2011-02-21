f1a9a1e70e430f8f24be8d3f87824b20e75127ba
Mike Orr <sluggoster@gmail.com>
2011-02-20 21:17:23 -0800

    Delete non-SQLAlchemy tests. Adjust varnames.

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import unittest
 import sqlalchemy as sa
 from sqlalchemy.engine.base import Engine
 
-import pyramid_sqla as psa
+import pyramid_sqla as psqla
 
 class DBInfo(object):
     def __init__(self, dir, filename):
@@ -21,7 +21,7 @@ class PyramidSQLATestCase(unittest.TestCase):
         self.db3 = DBInfo(self.dir, "db3.sqlite")
 
     def tearDown(self):
-        psa.reset()
+        psqla.reset()
         shutil.rmtree(self.dir, True)
 
     if not hasattr(unittest.TestCase, "assertIsInstance"): # pragma: no cover
@@ -43,17 +43,17 @@ class PyramidSQLATestCase(unittest.TestCase):
 
 class TestAddEngine(PyramidSQLATestCase):
     def test_keyword_args(self):
-        engine = psa.add_engine(url=self.db1.url)
+        engine = psqla.add_engine(url=self.db1.url)
         self.assertIsInstance(engine, Engine)
 
     def test_simplest_settings(self):
         settings = {"sqlalchemy.url": self.db1.url}
-        engine = psa.add_engine(settings, prefix="sqlalchemy.")
+        engine = psqla.add_engine(settings, prefix="sqlalchemy.")
         self.assertIsInstance(engine, Engine)
 
     def test_existing_engine(self):
         e = sa.create_engine(self.db1.url)
-        engine = psa.add_engine(engine=e)
+        engine = psqla.add_engine(engine=e)
         self.assertIs(engine, e)
 
     def test_multiple_engines(self):
@@ -61,47 +61,47 @@ class TestAddEngine(PyramidSQLATestCase):
             "sqlalchemy.url": self.db1.url,
             "stats.url": self.db2.url,
             "foo": "bar"}
-        default = psa.add_engine(settings)
-        stats = psa.add_engine(settings, name="stats", prefix="stats.")
+        default = psqla.add_engine(settings)
+        stats = psqla.add_engine(settings, name="stats", prefix="stats.")
         # Can we retrieve the engines?
-        self.assertIs(psa.get_engine(), default)
-        self.assertIs(psa.get_engine("default"), default)
-        self.assertIs(psa.get_engine("stats"), stats)
+        self.assertIs(psqla.get_engine(), default)
+        self.assertIs(psqla.get_engine("default"), default)
+        self.assertIs(psqla.get_engine("stats"), stats)
         # Are the session binding and base binding set correctly?
-        self.assertIs(psa.get_session().bind, default)
-        self.assertIs(psa.get_base().metadata.bind, default)
+        self.assertIs(psqla.get_session().bind, default)
+        self.assertIs(psqla.get_base().metadata.bind, default)
 
     def test_multiple_engines_without_default(self):
         settings = {
             "db1.url": self.db1.url,
             "db2.url": self.db2.url,
             "foo": "bar"}
-        db1 = psa.add_engine(settings, name="db1", prefix="db1.")
-        db2 = psa.add_engine(settings, name="db2", prefix="db2.")
+        db1 = psqla.add_engine(settings, name="db1", prefix="db1.")
+        db2 = psqla.add_engine(settings, name="db2", prefix="db2.")
         # Can we retrieve the engines?
-        self.assertIs(psa.get_engine("db1"), db1)
-        self.assertIs(psa.get_engine("db2"), db2)
+        self.assertIs(psqla.get_engine("db1"), db1)
+        self.assertIs(psqla.get_engine("db2"), db2)
         # There should be no default engine
-        self.assertIs(psa.get_session().bind, None)
-        self.assertIs(psa.get_base().metadata.bind, None)
-        self.assertRaises(RuntimeError, psa.get_engine)
+        self.assertIs(psqla.get_session().bind, None)
+        self.assertIs(psqla.get_base().metadata.bind, None)
+        self.assertRaises(RuntimeError, psqla.get_engine)
 
 class TestDeclarativeBase(PyramidSQLATestCase):
     def test1(self):
         import transaction
-        Base = psa.get_base()
+        Base = psqla.get_base()
         class Person(Base):
             __tablename__ = "people"
             id = sa.Column(sa.Integer, primary_key=True)
             first_name = sa.Column(sa.Unicode(100), nullable=False)
             last_name = sa.Column(sa.Unicode(100), nullable=False)
-        psa.add_engine(url=self.db1.url)
+        psqla.add_engine(url=self.db1.url)
         Base.metadata.create_all()
         fred = Person(id=1, first_name=u"Fred", last_name=u"Flintstone")
         wilma = Person(id=2, first_name=u"Wilma", last_name=u"Flintstone")
         barney = Person(id=3, first_name=u"Barney", last_name=u"Rubble")
         betty = Person(id=4, first_name=u"Betty", last_name=u"Rubble")
-        Session = psa.get_session()
+        Session = psqla.get_session()
         sess = Session()
         sess.add_all([fred, wilma, barney, betty])
         transaction.commit()
@@ -120,19 +120,19 @@ class TestDeclarativeBase(PyramidSQLATestCase):
         self.assertEqual(result, control)
 
     def test1_without_transaction_manager(self):
-        Base = psa.get_base()
+        Base = psqla.get_base()
         class Person(Base):
             __tablename__ = "people"
             id = sa.Column(sa.Integer, primary_key=True)
             first_name = sa.Column(sa.Unicode(100), nullable=False)
             last_name = sa.Column(sa.Unicode(100), nullable=False)
-        psa.add_engine(url=self.db1.url)
+        psqla.add_engine(url=self.db1.url)
         Base.metadata.create_all()
         fred = Person(id=1, first_name=u"Fred", last_name=u"Flintstone")
         wilma = Person(id=2, first_name=u"Wilma", last_name=u"Flintstone")
         barney = Person(id=3, first_name=u"Barney", last_name=u"Rubble")
         betty = Person(id=4, first_name=u"Betty", last_name=u"Rubble")
-        Session = psa.get_session()
+        Session = psqla.get_session()
         Session.configure(extension=None)  # XXX Kludge for SQLAlchemy/ZopeTransactionExtension bug
         sess = Session()
         sess.add_all([fred, wilma, barney, betty])
@@ -151,96 +151,26 @@ class TestDeclarativeBase(PyramidSQLATestCase):
         control = [u"Wilma", u"Fred", u"Betty", u"Barney"]
         self.assertEqual(result, control)
 
-
-class TestAddStaticRoute(unittest.TestCase):
-    def _callFUT(self, config, package, subdir, cache_max_age=3600,
-                 **add_route_args):
-        from pyramid_sqla.static import add_static_route
-        return add_static_route(config, package, subdir,
-                                cache_max_age=cache_max_age, **add_route_args)
-
-    def test_pattern_is_bad_arg(self):
-        self.assertRaises(TypeError, self._callFUT,
-                          None, None, None, pattern='foo')
-
-    def test_view_is_bad_arg(self):
-        self.assertRaises(TypeError, self._callFUT,
-                          None, None, None, view='foo')
-
-    def test_has_name(self):
-        from pyramid_sqla.static import StaticViewPredicate
-        from pyramid.view import static
-        config = DummyConfig()
-        self._callFUT(config, 'package', 'subdir', name='myname')
-        self.assertEqual(len(config.routes), 1)
-        route = config.routes[0]
-        self.assertEqual(route['pattern'], '/*subpath')
-        self.assertEqual(route['name'], 'myname')
-        self.assertEqual(route['kw']['custom_predicates'][0].__class__,
-                         StaticViewPredicate)
-        self.assertEqual(route['kw']['view'].__class__,
-                         static)
-
-    def test_has_no_name(self):
-        config = DummyConfig()
-        self._callFUT(config, 'package', 'subdir')
-        self.assertEqual(len(config.routes), 1)
-        route = config.routes[0]
-        self.assertEqual(route['pattern'], '/*subpath')
-        self.assertEqual(route['name'], 'static')
-
-class TestStaticViewPredicate(unittest.TestCase):
-    def _makeOne(self, package, subdir):
-        from pyramid_sqla.static import StaticViewPredicate
-        return StaticViewPredicate(package, subdir)
-
-    def test___call___has_no_subpath(self):
-        inst = self._makeOne('package', 'subdir')
-        self.assertEqual(inst({'match':{'subpath':()}}, None), False)
-
-    def test___call___resource_exists(self):
-        inst = self._makeOne('pyramid_sqla', 'tests')
-        self.assertEqual(inst({'match':{'subpath':('test.py',)}}, None), True)
-
-    def test___call___resource_doesnt_exist(self):
-        inst = self._makeOne('pyramid_sqla', 'tests')
-        self.assertEqual(inst({'match':{'subpath':('wont.py',)}}, None), False)
-
-class Test_includeme(unittest.TestCase):
-    def _callFUT(self, config):
-        from pyramid_sqla import includeme
-        return includeme(config)
-
-    def test_it(self):
-        from pyramid_sqla.static import add_static_route
-        config = DummyConfig()
-        self._callFUT(config)
-        self.assertEqual(config.directives['add_static_route'],
-                         add_static_route)
-
 class Test_add_engine(unittest.TestCase):
     def setUp(self):
         self.engines = {}
         self.session = DummySession()
         self.base = DummyBase()
-        import pyramid_sqla
-        self.old_engines = pyramid_sqla._engines
-        self.old_base = pyramid_sqla._base
-        self.old_session = pyramid_sqla._session
-        pyramid_sqla._engines = self.engines
-        pyramid_sqla._base = self.base
-        pyramid_sqla._session = self.session
+        self.old_engines = psqla._engines
+        self.old_base = psqla._base
+        self.old_session = psqla._session
+        psqla._engines = self.engines
+        psqla._base = self.base
+        psqla._session = self.session
 
     def tearDown(self):
-        import pyramid_sqla
-        pyramid_sqla._engines = self.old_engines
-        pyramid_sqla._base = self.old_base
-        pyramid_sqla._session = self.old_session
+        psqla._engines = self.old_engines
+        psqla._base = self.old_base
+        psqla._session = self.old_session
 
     def _callFUT(self, settings=None, name='default', prefix='sqlalchemy.',
                  engine=None, **engine_args):
-        from pyramid_sqla import add_engine
-        return add_engine(
+        return psqla.add_engine(
             settings=settings, name=name, prefix=prefix, engine=engine,
             **engine_args)
 
@@ -297,14 +227,3 @@ class DummyBase(object):
 
 class DummyEngine(object):
     pass
-        
-class DummyConfig(object):
-    def __init__(self):
-        self.routes = []
-        self.directives = {}
-
-    def add_route(self, name, pattern, **kw):
-        self.routes.append({'name':name, 'pattern':pattern, 'kw':kw})
-
-    def add_directive(self, name, value):
-        self.directives[name] = value
