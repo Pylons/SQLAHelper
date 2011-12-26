67aa6d868b864417705d87a0a4a54ecd66cec064
Mike Orr <sluggoster@gmail.com>
2011-12-25 22:41:28 -0800

    Bump version to 1.0.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ entry_points = """
 """
 
 setup(name='SQLAHelper',
-      version='1.0b1',
+      version='1.0',
       description='A convenience library for SQLAlchemy web applications',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
